05f58ca59a445c66989fc0753a1234f9ee6fc44a
tallbl0nde <40382856+tallbl0nde@users.noreply.github.com>
2020-02-19 19:53:34 +1030

    Change method of setting active menu option

--- source/horizon/menu/Menu.cpp
+++ source/horizon/menu/Menu.cpp
@@ -2,6 +2,7 @@
 
 namespace Aether {
     Menu::Menu(int x, int y, int w, int h) : Scrollable(x, y, w, h) {
+        this->active = nullptr;
         this->setShowScrollBar(false);
         this->setCatchup(15);
     }
@@ -14,6 +15,20 @@ namespace Aether {
             int sMid = this->y() + this->h()/2;
             int cMid = this->focussed()->y() + (this->focussed()->h()/2);
             this->setScrollPos(this->scrollPos + (this->scrollCatchup * (cMid - sMid) * (dt/1000.0)));
+        }
+    }
+
+    void Menu::setActiveOption(MenuOption * o) {
+        // Deactivate activated option
+        if (this->active != nullptr) {
+            this->active->setActive(false);
+        }
+
+        // Search children for option and activate
+        std::vector<Element *>::iterator it = std::find(this->children.begin(), this->children.end(), o);
+        if (it != this->children.end()) {
+            this->active = static_cast<MenuOption *>(*it);
+            this->active->setActive(true);
         }
     }
 };
