e297b983b598bf94b0c1d7fa4ba4aea0ab2093e1
eXhumer <exhumer1@protonmail.com>
2020-05-05 05:57:09 -0600

    Add doc for Overlay + change in close() function

--- include/Overlay.hpp
+++ include/Overlay.hpp
@@ -4,22 +4,39 @@
 #include "Screen.hpp"
 
 namespace Aether {
-    // An overlay is just a screen but with a transparent
-    // black background.
+    /**
+     * @brief An object representing an overlay.
+     * 
+     * Overlay is just a screem with a transparent black
+     * background.
+     */
     class Overlay : public Screen {
         private:
-            // Set true when it should be closed
+            /** @brief Store value to check if overlay should be closed or not. */
             bool close_;
 
         public:
+            /**
+             * @brief Construct a new Overlay object
+             */
             Overlay();
 
-            // Call to mark the overlay to be closed
-            void close(bool);
-            // Returns close
+            /**
+             * @brief Mark to overlay to be closed
+             */
+            void close();
+
+            /**
+             * @brief Check if overlay is marked to be closed
+             * 
+             * @return true if marked to close
+             * @return false 
+             */
             bool shouldClose();
 
-            // Render calls Screen but draws background first
+            /**
+             * @brief Draws background then renders screen
+             */
             void render();
     };
 };
