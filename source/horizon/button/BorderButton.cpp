2c6ac52ae8a21882b5335f20818bebee1658748a
tallbl0nde <40382856+tallbl0nde@users.noreply.github.com>
2020-12-08 21:45:09 +1030

    Scroll text if it doesn't fit inside buttons

--- source/horizon/button/BorderButton.cpp
+++ source/horizon/button/BorderButton.cpp
@@ -2,6 +2,8 @@
 
 // Corner radius of rectangle
 #define CORNER_RAD 8
+// Horizontal padding for text
+#define PADDING 20
 
 namespace Aether {
     BorderButton::BorderButton(int x, int y, int w, int h, unsigned int b, std::string t, unsigned int s, std::function<void()> f) : Element(x, y, w, h) {
@@ -9,6 +11,8 @@ namespace Aether {
         this->text = new Text(this->x() + this->w()/2, this->y() + this->h()/2, t, s);
         this->text->setXY(this->text->x() - this->text->w()/2, this->text->y() - this->text->h()/2);
         this->text->setScroll(true);
+        this->text->setScrollSpeed(40);
+        this->text->setScrollWaitTime(1500);
         this->addElement(this->box);
         this->addElement(this->text);
         this->setCallback(f);
@@ -36,22 +40,22 @@ namespace Aether {
 
     void BorderButton::setString(std::string s) {
         this->text->setString(s);
-        if (this->text->texW() > this->w()) {
-            this->text->setW(this->w());
+        if (this->text->texW() > this->w() - 1.5*PADDING) {
+            this->text->setW(this->w() - 2*PADDING);
         }
-        this->text->setXY(this->x() + this->w()/2 - this->text->w()/2, this->y() + this->h()/2 - this->text->h()/2);
+        this->text->setXY(this->x() + (this->w() - this->text->w())/2, this->y() + (this->h() - this->text->h())/2);
     }
 
     void BorderButton::setW(int w) {
         Element::setW(w);
         this->box->setBoxSize(this->w(), this->h());
-        this->text->setX(this->x() + this->w()/2 - this->text->w()/2);
+        this->setString(this->text->string());
     }
 
     void BorderButton::setH(int h) {
         Element::setH(h);
         this->box->setBoxSize(this->w(), this->h());
-        this->text->setY(this->y() + this->h()/2 - this->text->h()/2);
+        this->text->setY(this->y() + (this->h() - this->text->h())/2);
     }
 
     SDL_Texture * BorderButton::renderHighlightBG() {
--- source/horizon/button/FilledButton.cpp
+++ source/horizon/button/FilledButton.cpp
@@ -2,6 +2,8 @@
 
 // Corner radius of rectangle
 #define CORNER_RAD 8
+// Horizontal padding for text
+#define PADDING 20
 
 namespace Aether {
     FilledButton::FilledButton(int x, int y, int w, int h, std::string t, unsigned int s, std::function<void()> f) : Element(x, y, w, h) {
@@ -9,6 +11,8 @@ namespace Aether {
         this->text = new Text(this->x() + this->w()/2, this->y() + this->h()/2, "", s);
         this->setString(t);
         this->text->setScroll(true);
+        this->text->setScrollSpeed(40);
+        this->text->setScrollWaitTime(1500);
         this->addElement(this->rect);
         this->addElement(this->text);
         this->setCallback(f);
@@ -36,22 +40,22 @@ namespace Aether {
 
     void FilledButton::setString(std::string s) {
         this->text->setString(s);
-        this->text->setXY(this->rect->x() + (this->rect->w() - this->text->w())/2, this->rect->y() + (this->rect->h() - this->text->h())/2);
-        if (this->text->texW() > this->w()) {
-            this->text->setW(this->w());
+        if (this->text->texW() > this->w() - 1.5*PADDING) {
+            this->text->setW(this->w() - 2*PADDING);
         }
+        this->text->setXY(this->x() + (this->w() - this->text->w())/2, this->y() + (this->h() - this->text->h())/2);
     }
 
     void FilledButton::setW(int w) {
         Element::setW(w);
         this->rect->setRectSize(this->w(), this->h());
-        this->text->setX(this->x() + this->w()/2 - this->text->w()/2);
+        this->setString(this->text->string());
     }
 
     void FilledButton::setH(int h) {
         Element::setH(h);
         this->rect->setRectSize(this->w(), this->h());
-        this->text->setY(this->y() + this->h()/2 - this->text->h()/2);
+        this->text->setY(this->y() + (this->h() - this->text->h())/2);
     }
 
     SDL_Texture * FilledButton::renderHighlightBG() {
