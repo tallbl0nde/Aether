4ba706cb17dddbaefd93bb497e9a1d96bf7d6d4c
tallbl0nde <40382856+tallbl0nde@users.noreply.github.com>
2021-04-12 11:33:01 +0930

    Update ImageData to rely on Colours instead of bytes

--- include/Aether/types/ImageData.hpp
+++ include/Aether/types/ImageData.hpp
@@ -10,15 +10,11 @@ namespace Aether {
      * @brief Represents an object containing the raw pixel data for an image/texture
      * as an array, along with required metadata such as width and height.
      *
-     * @note When interfacing with this class using 'pixels' instead of bytes, only RGB
-     * and RGBA (i.e. 3 or 4 channels) are supported.
+     * @note Only RGB and RGBA (i.e. 3 or 4 channels) are supported.
      */
     class ImageData {
         private:
-            std::vector<uint8_t> bytes;         /** @brief Vector of bytes from top-left to bottom-right */
-            std::vector<Colour> pixels;         /** @brief Vector of pixels from top-left to bottom-right */
-            bool isBytes;                       /** @brief Flag indicating if we should use the vector of bytes */
-
+            std::vector<Colour> pixels_;        /** @brief Vector of pixels from top-left to bottom-right */
             size_t width_;                      /** @brief Width of image in pixels */
             size_t height_;                     /** @brief Height of image in pixels */
             uint8_t channels_;                  /** @brief The number of channels in the image (usually 3 or 4) */
@@ -54,21 +50,21 @@ namespace Aether {
              *
              * @return true if the stored image and metadata are valid values, false wtherwise.
              */
-            bool valid();
+            bool valid() const;
 
             /**
              * @brief Returns the width of the stored image in pixels.
              *
              * @return Width of the stored image in pixels.
              */
-            size_t width();
+            size_t width() const;
 
             /**
              * @brief Returns the height of the stored image in pixels.
              *
              * @return Height of the stored image in pixels.
              */
-            size_t height();
+            size_t height() const;
 
             /**
              * @brief Returns the number of channels used within the stored image.
@@ -76,7 +72,16 @@ namespace Aether {
              *
              * @return Number of channels used in the stored image.
              */
-            uint8_t channels();
+            uint8_t channels() const;
+
+            /**
+             * @brief Returns a pointer to the colour of a pixel at the given coordinate.
+             *
+             * @param x x-coordinate of pixel
+             * @param y y-coordinate of pixel
+             * @return Pointer to pixel colour, or nullptr if outside of image
+             */
+            Colour * colourAt(const size_t x, const size_t y) const;
 
             /**
              * @brief Returns the stored image as a vector of bytes (i.e. one byte per channel).
@@ -84,14 +89,14 @@ namespace Aether {
              *
              * @return Vector of image pixels in raw byte format.
              */
-            std::vector<uint8_t> toByteVector();
+            std::vector<uint8_t> toByteVector() const;
 
             /**
-             * @brief Returns the stored image as a vector of pixels.
+             * @brief Returns the stored image as a vector of colours.
              *
              * @return Vector of image pixels.
              */
-            std::vector<Colour> toPixelVector();
+            std::vector<Colour> toColourVector() const;
     };
 };
 
--- source/types/ImageData.cpp
+++ source/types/ImageData.cpp
@@ -2,60 +2,74 @@
 
 namespace Aether {
     ImageData::ImageData() {
-        this->isBytes = true;
-        this->bytes.resize(0);
+        this->pixels_.resize(0);
         this->width_ = 0;
         this->height_ = 0;
         this->channels_ = 0;
     }
 
     ImageData::ImageData(const std::vector<uint8_t> & data, const size_t width, const size_t height, const uint8_t channels) {
-        this->isBytes = true;
-        this->bytes = data;
+        // Convert bytes to pixels
+        for (size_t i = 0; i < data.size(); i += channels) {
+            this->pixels_.push_back(Colour(data[i], data[i+1], data[i+2], data[i+3]));
+        }
+
+        // Pad if necessary with opaque white
+        if (this->pixels_.size() < width * height) {
+            this->pixels_.resize(width * height, Colour(255, 255, 255, 255));
+        }
+
         this->width_ = width;
         this->height_ = height;
         this->channels_ = channels;
     }
 
     ImageData::ImageData(const std::vector<Colour> & pixels, const size_t width, const size_t height, const uint8_t channels) {
-        this->isBytes = false;
-        this->pixels = pixels;
+        this->pixels_ = pixels;
+
+        // Pad if necessary with opaque white
+        if (this->pixels_.size() < width * height) {
+            this->pixels_.resize(width * height, Colour(255, 255, 255, 255));
+        }
+
         this->width_ = width;
         this->height_ = height;
         this->channels_ = channels;
     }
 
-    bool ImageData::valid() {
-        if (this->isBytes) {
-            return !this->bytes.empty();
-        } else {
-            return !this->pixels.empty();
-        }
+    bool ImageData::valid() const {
+        return !this->pixels_.empty();
     }
 
-    size_t ImageData::width() {
+    size_t ImageData::width() const {
         return this->width_;
     }
 
-    size_t ImageData::height() {
+    size_t ImageData::height() const {
         return this->height_;
     }
 
-    uint8_t ImageData::channels() {
+    uint8_t ImageData::channels() const {
         return this->channels_;
     }
 
-    std::vector<uint8_t> ImageData::toByteVector() {
-        // Simply return copy if same format
-        if (this->isBytes) {
-            return this->bytes;
+    Colour * ImageData::colourAt(const size_t x, const size_t y) const {
+        // Check within range
+        size_t idx = (this->width_ * y) + x;
+        if (idx >= this->pixels_.size()) {
+            return nullptr;
         }
 
-        // Otherwise iterate over and convert
-        std::vector<uint8_t> bytes(this->pixels.size() * this->channels_);
-        for (size_t idx = 0; idx < this->pixels.size(); idx++) {
+        Colour & colour = const_cast<ImageData *>(this)->pixels_[idx];
+        return &colour;
+    }
+
+    std::vector<uint8_t> ImageData::toByteVector() const {
+        // Convert from pixels to bytes
+        std::vector<uint8_t> bytes(this->pixels_.size() * this->channels_);
+        for (size_t idx = 0; idx < this->pixels_.size(); idx++) {
             size_t byteIdx = idx * this->channels_;
-            Colour & pixel = this->pixels[idx];
+            Colour pixel = this->pixels_[idx];
 
             bytes[byteIdx + 0] = pixel.r();
             bytes[byteIdx + 1] = pixel.g();
@@ -68,19 +82,7 @@ namespace Aether {
         return bytes;
     }
 
-    std::vector<Colour> ImageData::toPixelVector() {
-        // Simply return copy if same format
-        if (this->isBytes) {
-            return this->pixels;
-        }
-
-        // Otherwise iterate over and convert
-        std::vector<Colour> pixels(this->bytes.size() / this->channels_);
-        for (size_t byteIdx = 0; byteIdx < this->bytes.size(); byteIdx += this->channels_) {
-            Colour colour = Colour(this->bytes[byteIdx], this->bytes[byteIdx + 1], this->bytes[byteIdx + 2], (this->channels_ > 3 ? this->bytes[byteIdx + 3] : 255));
-            pixels[byteIdx/this->channels_] = colour;
-        }
-
-        return pixels;
+    std::vector<Colour> ImageData::toColourVector() const {
+        return this->pixels_;
     }
 };
